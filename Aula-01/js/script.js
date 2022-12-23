c48b82127b7c56f4db9e76a441904c9598406686
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-12-23 07:41:48 -0300

    Finalizando a aula-1, introdução ao canvas com javascript

---
+++ Aula-01/js/script.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("desenho01")
+let ctx = canvas.getContext('2d')
+
+ctx.fillStyle = "#00f"
+ctx.fillRect(0, 0, 100, 100)
+
+ctx.fillStyle = "rgba(255,0,0,0.5)"
+ctx.fillRect(50, 50, 100, 100)
