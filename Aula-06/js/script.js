78b9fdc413d8d11de791050e3256d7d907789f72
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-01-30 14:44:03 -0300

    Atualizando aula 6

---
+++ Aula-06/js/script.js
@@ -0,0 +1,51 @@
+let canvas = document.getElementById("desenho01")
+let ctx = canvas.getContext('2d')
+let px = 0
+let py = 0
+let largura= 50
+let altura = 50
+let largura_canvas = 500
+let altura_canvas = 500
+let anima 
+let direcao_x = 1
+let direcao_y = 0
+
+desenha()
+
+function desenha(){
+    ctx.clearRect(0, 0, largura_canvas, altura_canvas)
+    ctx.fillStyle = '#f00'
+    ctx.lineWidth=3
+    ctx.stokeStyle="#000"
+
+    ctx.fillRect(px, py, largura , altura)
+    ctx.strokeRect(px, py, largura , altura)
+       
+    px += direcao_x
+    py += direcao_y
+
+    if ((px <= 0) && (py <= 0)){
+        direcao_x = 1
+        direcao_y = 0
+    }
+
+    if ((px + largura >= largura_canvas) && (py <= 0)){
+        direcao_x = 0
+        direcao_y = 1
+    }
+
+  //  if ((px + largura >= largura_canvas) && (py+ altura_canvas >= altura_canvas)){
+  //      direcao_x =  0
+   //     direcao_y = -1
+  //  }
+
+  //  if ((px <= 0) && (py + altura >= altura_canvas)){
+  //      direcao_x = 0
+  //      direcao_y = -1
+ //   }
+
+
+       
+   
+    anima = requestAnimationFrame(desenha)
+}
